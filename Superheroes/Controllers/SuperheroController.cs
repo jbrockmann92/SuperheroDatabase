0bcbf38753dbe082e9e98b5bea94e27fb8f9c8b6
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-02-18 05:05:54 -0800

    Took one last look through to make sure things look good, and removed comments

--- Superheroes/Controllers/SuperheroController.cs
+++ Superheroes/Controllers/SuperheroController.cs
@@ -19,30 +19,23 @@ namespace Superheroes.Controllers
             _context = context;
         }
 
-        // GET: Superhero
         public ActionResult Index()
         {
             return View(_context.Superheroes.ToList());
-            //Will return the html page called Index from the Views/Superhero folder??
-            //Returns the homepage?
         }
 
-        // GET: Superhero/Details/5
         public ActionResult Details(int id)
         {
             var hero = _context.Superheroes.Where(s => s.Id == id).FirstOrDefault();
             return View(hero);
-            //Need to get working so that I can click on his name, not the Details button
         }
 
-        // GET: Superhero/Create
         public ActionResult Create()
         {
             Superhero superhero = new Superhero();
             return View(superhero);
         }
 
-        // POST: Superhero/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Superhero superhero)
@@ -66,18 +59,15 @@ namespace Superheroes.Controllers
             return View();
         }
 
-        // GET: Superhero/Edit/5
         public ActionResult Edit(int id)
         {
             Superhero superhero = _context.Superheroes.Where(s => s.Id == id).FirstOrDefault();
-            //I don't think I need this
             return View(superhero);
         }
 
-        // POST: Superhero/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, Superhero superhero) //What is this collection up to? What's it for?
+        public ActionResult Edit(int id, Superhero superhero)
         {
                 if (ModelState.IsValid)
                 {
@@ -91,7 +81,6 @@ namespace Superheroes.Controllers
                 }
         }
 
-        // GET: Superhero/Delete/5
         public ActionResult Delete(int id)
         {
             var hero = _context.Superheroes.Where(s => s.Id == id).FirstOrDefault();
@@ -100,15 +89,12 @@ namespace Superheroes.Controllers
             return View();
         }
 
-        // POST: Superhero/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
         {
             try
             {
-                // TODO: Add delete logic here
-
                 return RedirectToAction(nameof(Index));
             }
             catch
